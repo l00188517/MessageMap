fc151dbff7c531740f739f8978c0122a67889b7a
Mathieu <L00188517@atu.ie>
2025-04-06 04:27:50 +0100

    Added MessageMap MVP v1

---
+++ v1/service-worker.js
@@ -0,0 +1,62 @@
+const staticAssets = [
+    './',
+    './index.html',
+    './about.html',
+    './manifest.json',
+    './icon-192x192.png',
+    './icon-512x512.png',
+    'https://unpkg.com/leaflet@1.7.1/dist/leaflet.css',
+    'https://unpkg.com/leaflet@1.7.1/dist/leaflet.js'
+];
+
+self.addEventListener('install', async event => {
+    const cache = await caches.open('static-assets');
+    cache.addAll(staticAssets);
+});
+
+self.addEventListener('fetch', event => {
+    const req = event.request;
+    const url = new URL(req.url);
+
+    if (url.origin === location.origin) {
+        event.respondWith(cacheFirst(req));
+    } else {
+        event.respondWith(networkFirst(req));
+    }
+});
+
+async function cacheFirst(req) {
+    const cachedResponse = await caches.match(req);
+    return cachedResponse || fetch(req);
+}
+
+async function networkFirst(req) {
+    const cache = await caches.open('dynamic-assets');
+    try {
+        if (req.url.startsWith('chrome-extension://')) {
+            return fetch(req); // Bypass caching for extension requests
+        }
+
+        const res = await fetch(req);
+        cache.put(req, res.clone());
+        return res;
+    } catch (error) {
+        return await cache.match(req);
+    }
+}
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          // Delete old caches
+          if (cacheName !== 'static-assets' && cacheName !== 'dynamic-assets') {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+  self.clients.claim(); // Activate new service worker immediately
+});
